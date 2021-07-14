1a820400b331de2249d640d4d2c5e5f0e7bbb4f0
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-07-14 14:57:30 -0300

    feat: adding user login

--- src/components/AuthForm/index.js
+++ src/components/AuthForm/index.js
@@ -1,6 +1,6 @@
-import { useRef, useState } from 'react';
+import { useRef, useState } from "react";
 
-import { Container, Control, Actions, ToggleButton } from './styles';
+import { Container, Control, Actions, ToggleButton } from "./styles";
 
 export function AuthForm() {
   const emailInputRef = useRef();
@@ -11,7 +11,7 @@ export function AuthForm() {
 
   function handleSwitchAuthMode() {
     setIsLogin((prevState) => !prevState);
-  };
+  }
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -20,62 +20,76 @@ export function AuthForm() {
     const password = passwordInputRef.current.value;
 
     setIsLoading(true);
-    
+
+    let url;
+
     if (isLogin) {
-      
+      url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBIfWrde8TkiZmWittDscqthJfUMkVswIU`;
     } else {
-      fetch(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBIfWrde8TkiZmWittDscqthJfUMkVswIU`, {
-        method: 'POST',
+      url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBIfWrde8TkiZmWittDscqthJfUMkVswIU`;
+    }
+    fetch(
+      url,
+      {
+        method: "POST",
         body: JSON.stringify({
           email,
           password,
           returnSecureToken: true,
         }),
         headers: {
-          'Content-Type': 'application/json',
-          
-        }
-      }).then(res => {
-        setIsLoading(false);
-        if(res.ok) {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then((res) => {
+      setIsLoading(false);
+      if (res.ok) {
+        return res.json();
+      } else {
+        return res.json().then((data) => {
+          let errorMessage = "Authentication failed!";
 
-        } else {
-          return res.json().then(data => {
-            let errorMessage = "Authentication failed!";
+          // if(data && data.error && data.error.message) errorMessage = data.error.message;
 
-            // if(data && data.error && data.error.message) errorMessage = data.error.message;
-
-            alert(errorMessage);
-          })
-        }
-      })
-    };
-  };
+          throw new Error(errorMessage);
+        });
+      }
+    })
+    .then((data) => {
+      console.log(data)
+    })
+    .catch((err) => {
+      alert(err.message)
+    })
+  }
 
   return (
     <Container>
-       <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
+      <h1>{isLogin ? "Login" : "Sign Up"}</h1>
       <form onSubmit={handleSubmit}>
         <Control>
-          <label htmlFor='email'>Your Email</label>
-          <input ref={emailInputRef} type='email' id='email' required />
+          <label htmlFor="email">Your Email</label>
+          <input ref={emailInputRef} type="email" id="email" required />
         </Control>
         <Control>
-          <label htmlFor='password'>Your Password</label>
-          <input ref={passwordInputRef} type='password' id='password' required />
+          <label htmlFor="password">Your Password</label>
+          <input
+            ref={passwordInputRef}
+            type="password"
+            id="password"
+            required
+          />
         </Control>
         <Actions>
-          {!isLoading && <button>{isLogin ? 'Login' : 'Create Account'}</button>}
+          {!isLoading && (
+            <button>{isLogin ? "Login" : "Create Account"}</button>
+          )}
           {isLoading && <p>Sending request...</p>}
-          <ToggleButton
-            type='button'
-            onClick={handleSwitchAuthMode}
-          >
-            {isLogin ? 'Create new account' : 'Login with existing account'}
+          <ToggleButton type="button" onClick={handleSwitchAuthMode}>
+            {isLogin ? "Create new account" : "Login with existing account"}
           </ToggleButton>
         </Actions>
       </form>
     </Container>
   );
-};
-
+}
