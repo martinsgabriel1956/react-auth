ac23b3b45fbd2c9ebc0778614bc8057234cb842d
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-07-14 17:00:33 -0300

    feat: protecting frontend pages

--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,29 @@
-import { Switch, Route } from "react-router-dom";
+import { useContext } from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
 
 import { Home } from "./pages/Home";
 import { Auth } from "./pages/Auth";
 import { Profile } from "./pages/Profile";
 
 import { Layout } from "./components/Layout";
+import { AuthContext } from "./context/AuthContext";
 
 export function Routes() {
+  const authCtx = useContext(AuthContext);
+
   return (
     <>
       <Layout>
         <Switch>
           <Route path="/" exact component={Home} />
-          <Route path="/auth" component={Auth} />
-          <Route path="/profile" component={Profile} />
+          {!authCtx.isLoggedIn && <Route path="/auth" component={Auth} />}
+          <Route path="/profile">
+            {authCtx.isLoggedIn && <Profile />}
+            {!authCtx.isLoggedIn && <Redirect to="/auth" />}
+          </Route>
+          <Route path="*">
+            <Redirect to="/" />
+          </Route>
         </Switch>
       </Layout>
     </>
