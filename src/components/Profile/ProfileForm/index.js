1eb0b76522e660af92167afa7ac534dd9a635d9f
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-07-14 16:21:17 -0300

    feat: using the token for requests to protected resources

--- src/components/Profile/ProfileForm/index.js
+++ src/components/Profile/ProfileForm/index.js
@@ -1,11 +1,40 @@
+import { useContext, useRef } from "react";
+import { AuthContext } from "../../../context/AuthContext";
+
 import { Container, Control, Action } from "./styles";
 
 export function ProfileForm() {
+  const newPasswordInputRef = useRef();
+  const { token } = useContext(AuthContext);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const newPassword = newPasswordInputRef.current.value;
+
+    // add validation
+
+    fetch('https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyBIfWrde8TkiZmWittDscqthJfUMkVswIU', {
+      method: 'POST',
+      body: JSON.stringify({
+        idToken: token,
+        password: newPassword,
+        returnSecureToken: false
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    }).then(res => {
+
+    })
+
+  };
+
   return (
-    <Container>
+    <Container onSubmit={handleSubmit}>
       <Control>
         <label htmlFor="new-password">New Password</label>
-        <input type="password" id="new-password" />
+        <input ref={ newPasswordInputRef } type="password" minLength={7} id="new-password" />
       </Control>
       <Action>
         <button>Change Password</button>
